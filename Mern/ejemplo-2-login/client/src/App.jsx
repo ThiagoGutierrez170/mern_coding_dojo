dc17cbac3e47529cf7e62f25deb919ea8aa9a5cb
Thiago Gutierrez <thiagogutierrez112@gmail.com>
2024-10-14 21:09:35 -0300

    mejoro un poco el login

--- Mern/ejemplo-2-login/client/src/App.jsx
+++ Mern/ejemplo-2-login/client/src/App.jsx
@@ -1,22 +1,34 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import RegisterPage from "./pages/RegisterPage.jsx"
-import UserPage from "./pages/UserPage.jsx"
-import UserDetailPage from "./pages/UserDetailPage.jsx"
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import UserPage from "./pages/UserPage.jsx";
+import UserDetailPage from "./pages/UserDetailPage.jsx";
+import TarjetaEstado from "./pages/TarjertaEstado.jsx";
+import RouteError from "./pages/RouteError.jsx";
 
-
-function App() {
+const NavigationBar = () => {
+  const navigate = useNavigate(); // Hook para la navegación programática
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={RegisterPage} />
-          <Route path="/users" Component={UserPage} />
-          <Route path="/users/:id" Component={UserDetailPage} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <nav className='navegacion'>
+      <button onClick={() => navigate('/')}>Inicio</button>
+      <button onClick={() => navigate('/estado')}>Estado</button>
+    </nav>
+  );
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavigationBar/>
+      <Routes>
+        <Route path="/" Component={RegisterPage} />
+        <Route path="/users" Component={UserPage} />
+        <Route path="/users/:id" Component={UserDetailPage} />
+        <Route path="/estado" Component={TarjetaEstado} />
+        <Route path="*" Component={RouteError} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
